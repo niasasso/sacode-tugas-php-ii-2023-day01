3c87e70d82dea3adde6dfc2926172a4cd42b0372
Sulenias Asso <74882893+niasasso@users.noreply.github.com>
2023-03-15 12:55:34 +0900

    Tambah echo-print.php

---
+++ echo-print.php
@@ -0,0 +1,12 @@
+<?php 
+
+	// contoh penggunaan echo
+	echo "<h1>Papua Tech Community</h1>";
+	// echo "<br>";
+	echo "<p>Berbagi dan belajar di Komunitas.</p>";
+
+	// contoh penggunaan print
+	print "<h1>Papua Coding School</h1>";
+	print "<p>Belajar coding dari dasar dengan mengikuti kursus.</p>";
+
+ ?>
---
+++ variables.php
@@ -0,0 +1,17 @@
+<?php 
+
+	// contoh penulisan variables di dalam file PHP
+	$title = "SaCode";
+	$tag_line = "Papua Coding School";
+	$tahun = "2023";
+
+	// Menampilkan data dalam variable
+	echo $title;
+	echo "<br>";
+
+	echo $tag_line;
+	echo "<br>";
+
+	echo $tahun;
+
+ ?>
